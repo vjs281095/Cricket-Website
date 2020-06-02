96066773d99de66dc2c1e6b3ea8b5fddc4ae4a44
vjs281095 <vjs281095@gmail.com>
2020-06-02 00:26:10 -0700

    upload

---
+++ Button_Script.js
@@ -0,0 +1,4 @@
+function redirect()
+{
+	document.getElementById("btn").onclick = "window.location.href='index.html";
+}
---
+++ Simple_Script.js
@@ -0,0 +1,5 @@
+function func1() {
+  alert("This is the main page");
+}
+window.onload=func1;
+
